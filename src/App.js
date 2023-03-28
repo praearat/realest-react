10ad6b46135667f9dcedfe455af739609a24d46c
Aratchaporn Jiamwijitkul <praearatchaporn@gmail.com>
2023-03-28 16:50:38 +0700

    Create the header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const matchLocation = (value) => {
+    if (value === location.pathname) {
+      return true;
+    }
+  };
+
+  return (
+    <div className=" bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className=" flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://propholic.com/wp-content/uploads/2018/02/asp-logo.jpg"
+            alt="logo"
+            className=" h-14 cursor-pointer"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+        </div>
+        <div>
+          <ul className=" flex space-x-10">
+            <li
+              className={` cursor-pointer py-4 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                matchLocation("/") && " text-black border-b-cyan-700"
+              }`}
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </li>
+            <li
+              className={` cursor-pointer py-4 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                matchLocation("/offers") && " text-black border-b-cyan-700"
+              }`}
+              onClick={() => {
+                navigate("/offers");
+              }}
+            >
+              Offers
+            </li>
+            <li
+              className={` cursor-pointer py-4 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                matchLocation("/sign-in") && " text-black border-b-cyan-700"
+              }`}
+              onClick={() => {
+                navigate("/sign-in");
+              }}
+            >
+              Sign In
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
