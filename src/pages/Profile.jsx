a4f59a027fa93bf784c1fb7867f7ec3160d91cbc
Aratchaporn Jiamwijitkul <praearatchaporn@gmail.com>
2023-04-05 15:59:52 +0700

    Add the edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { toast } from "react-toastify";
 
 const Profile = () => {
   const auth = getAuth();
@@ -10,10 +13,32 @@ const Profile = () => {
     email: auth.currentUser.email,
   });
   const { name, email } = userData;
+  const [selectEdit, setSelectEdit] = useState(false);
 
   const onSignOut = () => {
     auth.signOut();
     navigate("/");
+  };
+
+  const onClickApplyChange = async () => {
+    try {
+      if (name !== auth.currentUser.displayName) {
+        updateProfile(auth.currentUser, { displayName: name });
+        console.log(auth.currentUser);
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, { name: name });
+        toast.success("Profile was updated");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Could not update the profile");
+    }
+  };
+
+  const onChangeName = (event) => {
+    setUserData((prev) => {
+      return { ...prev, [event.target.id]: event.target.value };
+    });
   };
 
   return (
@@ -23,14 +48,19 @@ const Profile = () => {
         <div className="w-full md:w-[50%] px-5 mt-6">
           <form>
             <input
-              className="w-full px-5 py-3 rounded-md border-gray-300 text-sm text-gray-700 transition ease-in-out mb-3"
+              className={`w-full px-5 py-3 rounded-md  text-sm text-gray-700 transition ease-in-out mb-3 ${
+                selectEdit
+                  ? "bg-red-200 border-gray-300"
+                  : "bg-white border-white"
+              }`}
               type="text"
               id="name"
               value={name}
-              disabled
+              disabled={!selectEdit}
+              onChange={onChangeName}
             />
             <input
-              className="w-full px-5 py-3 rounded-md border-gray-300 text-sm text-gray-700 transition ease-in-out mb-3"
+              className="w-full px-5 py-3 rounded-md border-white text-sm text-gray-700 transition ease-in-out mb-3"
               type="email"
               id="email"
               value={email}
@@ -39,8 +69,16 @@ const Profile = () => {
             <div className="flex justify-between mb-3 whitespace-nowrap text-sm">
               <p className="flex ">
                 Do you want to change your name?
-                <p className="mx-1 text-red-500 hover:text-red-600 cursor-pointer transition duration-200 ease-in-out">
-                  Edit
+                <p
+                  className="ml-1 mr-2 text-red-500 hover:text-red-600 cursor-pointer transition duration-200 ease-in-out"
+                  onClick={() => {
+                    selectEdit && onClickApplyChange();
+                    setSelectEdit((prev) => {
+                      return !prev;
+                    });
+                  }}
+                >
+                  {!selectEdit ? "Edit" : "Apply change"}
                 </p>
               </p>
               <p
